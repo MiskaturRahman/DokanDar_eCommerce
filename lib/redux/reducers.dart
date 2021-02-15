9e0311b55cb0a843088b2b2516e749556f0bff7f
Misk007 <rahmnmisk007@gmail.com>
2021-02-15 16:01:23 +0600

    redux setup

---
+++ lib/redux/reducers.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_ecommerce/models/app_state.dart';
+
+AppState appReducer(state, action) {
+  return AppState(user: userReducer(state.user, action), cardToken: ''
+      // products: productsReducer(state.products, action),
+      // cartProducts: cartProducts(state.cartProducts, action),
+      // cards: cardsReducer(state.cards, action),
+      // cardToken: cardTokenReducer(state.cardToken, action),
+      // orders: ordersReducer(state.orders, action));
+      );
+}
+
+userReducer(user, action) {
+  // if (action is GetUserAction) {
+  //   return action.user;
+  // } else if (action is LogoutUserAction) {
+  //   return action.user;
+  // }
+  return user;
+}
